9ca0eb1191065a6d598784cd26cae44766a572b3
root <root@GFG0179-PRATEEK>
2023-04-01 12:10:07 +0530

    created teachers api

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -65,6 +65,8 @@ class Assignment(db.Model):
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.student_id == principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT, 'only a draft assignment can be submitted')
+        assertions.assert_valid(assignment.teacher_id is None, 'only a draft assignment can be submitted')
 
         assignment.teacher_id = teacher_id
         assignment.state = AssignmentStateEnum.SUBMITTED
@@ -73,5 +75,21 @@ class Assignment(db.Model):
         return assignment
 
     @classmethod
+    def gradeAssignment(cls, _id, gradeOfAssignment, principal: Principal):
+        assignment = Assignment.get_by_id(_id)
+        assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(assignment.teacher_id == principal.teacher_id, 'This assignment belongs to some other teacher')
+        assertions.assert_valid(gradeOfAssignment in ['A', 'B', 'C', 'D'], 'assignment with empty content cannot be submitted')
+        assignment.grade = gradeOfAssignment
+        assignment.state = AssignmentStateEnum.GRADED
+        db.session.flush()
+
+        return assignment
+
+    @classmethod
     def get_assignments_by_student(cls, student_id):
         return cls.filter(cls.student_id == student_id).all()
+
+    @classmethod
+    def get_assignments_by_teacher(cls, teacher_id):
+        return cls.filter(cls.teacher_id == teacher_id).all()
